28451054b1bb5800834adee011ce307d8c11651b
Mees Dekker <meesdekker2622002@gmail.com>
2021-09-13 14:27:43 +0200

    fixed fixed auth?

--- src/routes/app/auth.svelte
+++ src/routes/app/auth.svelte
@@ -1,6 +1,5 @@
 <script>
 	import {
-		Auth0Context,
 		Auth0LoginButton,
 		Auth0LogoutButton,
 		authError,
@@ -8,27 +7,18 @@
 		idToken,
 		isAuthenticated,
 		isLoading,
-		login,
-		logout,
 		userInfo
 	} from '@dopry/svelte-auth0';
 </script>
 
-<Auth0Context
-	domain="devenv1.eu.auth0.com"
-	client_id="ByHotM5JrhtGVsdOjmKgKVJItMox4sSz"
-	callback_url="https://deruiterstok.nl/app"
-	logout_url="https://deruiterstok.nl/app"
->
-	<div class="flex m-15">
-		<Auth0LoginButton class="btn">Login</Auth0LoginButton>
-		<Auth0LogoutButton class="btn">Logout</Auth0LogoutButton>
-	</div>
+<div class="flex m-15">
+	<Auth0LoginButton class="btn">Login</Auth0LoginButton>
+	<Auth0LogoutButton class="btn">Logout</Auth0LogoutButton>
+</div>
 
-	<pre>isLoading: {$isLoading}</pre>
-	<pre>isAuthenticated: {$isAuthenticated}</pre>
-	<pre>authToken: {$authToken}</pre>
-	<pre>idToken: {$idToken}</pre>
-	<pre>userInfo: {JSON.stringify($userInfo, null, 2)}</pre>
-	<pre>authError: {$authError}</pre>
-</Auth0Context>
+<pre>isLoading: {$isLoading}</pre>
+<pre>isAuthenticated: {$isAuthenticated}</pre>
+<pre>authToken: {$authToken}</pre>
+<pre>idToken: {$idToken}</pre>
+<pre>userInfo: {JSON.stringify($userInfo, null, 2)}</pre>
+<pre>authError: {$authError}</pre>
