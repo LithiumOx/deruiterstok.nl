647f062af4843197efc32c2e0bbad4d1e6ccf372
Mees Dekker <meesdekker2622002@gmail.com>
2021-10-28 17:21:54 +0200

    Now design is better :D

--- src/routes/word-lid.svelte
+++ src/routes/word-lid.svelte
@@ -7,30 +7,9 @@
 	class="hero min-h-[80vh] rounded-box bg-base-200 mt-4"
 	style="background-image: url({Background});background-repeat: repeat;"
 >
-	<div class="card bg-base-100 max-w-[95%] max-h-[95%] p-10">
-		<a class="w-1/5 btn btn-ghost" href="/">← Terug</a>
-		<h1 class="text-4xl font-extrabold mb-2.5 text-center text-base-content">Word nu lid!</h1>
-		<div class="my-2 alert alert-info">
-			<div class="flex-1">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					class="w-6 h-6 mx-2 stroke-current"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-					/>
-				</svg>
-				<label
-					>Wanneer wij de mail ontvangen sturen we je zo spoedig mogelijk een tikkie terug!</label
-				>
-			</div>
-		</div>
-		<div class="space-y-2 form-control">
+	<div class="card bg-base-100 max-w-[95%] max-h-[95%] p-2.5">
+		<h1 class="text-4xl font-extrabold my-2.5 text-center text-base-content">Word nu lid!</h1>
+		<div class="mx-2 space-y-2 mx-2.5 form-control">
 			<div>
 				<label class="label">
 					<span class="label-text">Volledige naam</span>
@@ -83,10 +62,31 @@
 						bind:value={Nummer}
 					/>
 				</div>
-				<div class="flex mt-5">
+				<div class="my-2.5 alert alert-info">
+					<div class="flex-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="w-6 h-6 mx-2 stroke-current"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<label
+							>Wanneer wij de mail ontvangen sturen we je zo spoedig mogelijk een tikkie terug!</label
+						>
+					</div>
+				</div>
+				<div class="flex mt-5 space-x-2">
+					<a class="w-1/3 btn" href="/">← Terug</a>
 					<a
 						href="mailto:ruiterstok@outlook.com?subject={Naam}%20{Achternaam}%20wil%20lid%20worden!&body=Naam%3A%20{Naam}%20{Achternaam}%0D%0AGeboortedatum%3A%20{Dag}-{Maand}-{Jaar}%0D%0ATelefoonnummer%3A%200{Nummer}"
-						class="self-end w-1/4 btn btn-primary">Mail ons!</a
+						class="self-end w-2/3 btn btn-primary">Mail ons!</a
 					>
 				</div>
 			</div>
@@ -96,6 +96,6 @@
 
 <style>
 	label {
-		@apply text-center font-semibold;
+		@apply text-left;
 	}
 </style>
